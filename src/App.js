1eb1d16477b4cfc978e88082268c655746045b92
sylvie <ngh.sylvie@gmail.com>
2018-10-04 13:25:44 +0200

    premier background avec bouton

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react'
 import Search from './Search'
 import './App.css'
 import Header from './Header/Header'
+import Background from "./background/background";
 
 
 
 class App extends Component {
+  state = {}
+
+photo = () => {
+  const arr = Object.keys(Background);
+  const randomarr = arr[Math.floor(Math.random()*arr.length)];
+  this.setState(Background[randomarr]);
+}
+
 
   render() {
     return (
       <div className="App">
         <Header />
         <Search />
-
+        <img src={this.state.image} className="bg_img"></img>
+        <br/>
+        <button onClick={test => this.photo(test)}>change de BG avec ce bouton :D</button>
       </div>
     );
   }
---
+++ src/background/background.js
@@ -0,0 +1,15 @@
+module.exports = {
+  image1:{
+    image:"https://images.unsplash.com/photo-1526214382099-9549c6938704?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=57ee8c4cd84a6b3ec67f430340b3af62&auto=format&fit=crop&w=1618&q=80"},
+ 
+  image2:{
+    image:"https://images.unsplash.com/photo-1471623320832-752e8bbf8413?ixlib=rb-0.3.5&s=005b1376e89624f720f6461749b34af4&auto=format&fit=crop&w=1549&q=80"},
+  
+  image3:{
+    image:"https://images.unsplash.com/photo-1451857652021-e406bc6bfea9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c3cbdd1d823c27eea2d1d5d1867b5c71&auto=format&fit=crop&w=1600&q=80"},
+  
+  image4:{
+    image:"https://images.unsplash.com/photo-1521458329725-b5fca46f0aea?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=179d0b555c2afaf86ff5ca45fc34d08c&auto=format&fit=crop&w=1534&q=80"},
+
+  image5:{
+    image:"https://images.unsplash.com/photo-1529835813023-cdecbabd6303?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=624c25a86fa27e3c996d312a36ee9626&auto=format&fit=crop&w=1534&q=80"}};
