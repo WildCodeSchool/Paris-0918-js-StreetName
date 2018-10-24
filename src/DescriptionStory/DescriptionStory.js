e725bffe68cdf609c0328813dfe574d39c671663
ThibltC <yojibo@hotmail.fr>
2018-10-24 15:57:42 +0200

    routing dynamique

--- src/DescriptionStory/DescriptionStory.js
+++ src/DescriptionStory/DescriptionStory.js
@@ -1,29 +1,65 @@
 import React from "react";
 import "./DescriptionStory.css";
+import { Link } from 'react-router-dom';
 
 class DescriptionStory extends React.Component {
+
+  state = {
+    dataInfos: undefined,
+    infoDisplay: false,
+  }
+
+  componentWillMount() {
+    fetch(`https://opendata.paris.fr/api/records/1.0/search/?dataset=voiesactuellesparis2012&q=${this.props.match.params.typo}`)
+      .then(data => data.json())
+      .then(data => this.setState({
+        dataInfos: data.records[0].fields,
+        infoDisplay: true
+      }))
+
+
+    // this.monumentsClasses()
+  }
+
   render() {
-    if (this.props.error)
+    if (!this.state.infoDisplay)
       return (
-        <div>
-          {" "}
-          <br />
-          Pas de correspondance trouvé.
+        <div className="noCorrespondence">
+          <Link to='/' >
+            <button
+              type="button"
+              onClick={this.clearState}
+            >
+              Nouvelle recherche
+        </button>
+          </Link>
+          <div>
+            {" "}
+            <br />
+            Pas de correspondance trouvé.
+    </div>
         </div>
-      );
-      
-    else
-      return (
-        <div>
-          {this.props.streetstory && (
-            <div className="result">
-              <h1 className="story"> HISTOIRE </h1> 
-              <h6> {this.props.streetstory} </h6>
-              Nom de la rue: <h2>{this.props.streetname}</h2>
-            </div>
-          )}
-        </div>
-      );
+      )
+
+    return (
+      <div className="DescriptionStory">
+        <Link to='/' >
+          <button
+            type="button"
+            onClick={this.clearState}
+          >
+            Nouvelle recherche
+          </button>
+        </Link>
+        {this.state.infoDisplay &&
+          <div className="result">
+            <h1 className="story"> HISTOIRE </h1>
+            <h6> {this.state.dataInfos.histo} </h6>
+            Nom de la rue: <h2>{this.state.dataInfos.typo}</h2>
+          </div>
+        }
+      </div>
+    );
   }
 }
 
