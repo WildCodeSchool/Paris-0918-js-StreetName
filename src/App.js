50d4523d403b655b39af9fe5a621c12149169222
Mathieu <promathieuthiry@gmail.com>
2018-10-25 18:09:09 +0200

    adding background

--- src/App.js
+++ src/App.js
@@ -12,12 +12,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="bgIMG">
       <Header />
         <Switch>
           <Route exact path="/" component={Search}/>
           <Route exact path="/map/:latlng(\d{1,3}.\d*,\d{1,3}.\d*)" component={Maps}/>
           <Route exact path="/:typo/:latlng(\d{1,3}.\d*,\d{1,3}.\d*)" component={DescriptionStory}/>
         </Switch>
+        </div>
       </div>
     );
   }
